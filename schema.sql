c7296777f1a590a810faf7a0d3db0abdcfae2f39
IngDavidTM <david-1011@hotmail.com>
2022-12-07 18:03:52 -0500

    add IDs

--- schema.sql
+++ schema.sql
@@ -11,4 +11,38 @@ CREATE TABLE animals(
 );
 
 ALTER TABLE animals
-  ADD species VARCHAR(250);
+ADD species VARCHAR(250);
+
+CREATE TABLE owners(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  full_name VARCHAR(250),
+  age INT,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE species(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(250),
+  PRIMARY KEY(id)
+);
+
+ALTER TABLE animals
+DROP COLUMN species;
+
+ALTER TABLE animals
+ADD species_id INT;
+
+ALTER TABLE animals
+ADD CONSTRAINT fk_species
+FOREIGN KEY (species_id)
+REFERENCES species(id)
+ON DELETE CASCADE;
+
+ALTER TABLE animals
+ADD owner_id INT;
+
+ALTER TABLE animals
+ADD CONSTRAINT fk_owners
+FOREIGN KEY (owner_id)
+REFERENCES owners(id)
+ON DELETE CASCADE;
