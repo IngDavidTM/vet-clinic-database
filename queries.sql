9b01b0f031fe8f70f7d3ddfa9a5c69dc2cbebd95
IngDavidTM <david-1011@hotmail.com>
2022-12-07 13:47:25 -0500

    change weights using transactions

--- queries.sql
+++ queries.sql
@@ -8,3 +8,33 @@ SELECT name, escape_attempts from animals WHERE weight_kg>10.5;
 SELECT * from animals WHERE neutered=true;
 SELECT * from animals WHERE name!='Gabumon';
 SELECT * from animals WHERE weight_kg BETWEEN 10.4 and 17.3;
+
+
+BEGIN;
+UPDATE animals SET species = 'unidentified';
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+COMMIT;
+SELECT * FROM animals;
+
+
+BEGIN;
+DELETE FROM animals;
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT check1;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO SAVEPOINT check1;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
+
+
