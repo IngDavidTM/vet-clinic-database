c95983a9a6151c78514eb7e7c2c155585c667c66
IngDavidTM <david-1011@hotmail.com>
2022-12-07 18:38:53 -0500

    add queries

--- queries.sql
+++ queries.sql
@@ -44,3 +44,10 @@ SELECT neutered, SUM(escape_attempts) FROM animals GROUP BY neutered;
 SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
 SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '1999-12-31' GROUP BY species;
 
+SELECT owners.full_name, name FROM animals INNER JOIN owners ON owner_id = owners.id WHERE owners.full_name = 'Melody Pond';
+SELECT species.name, animals.name FROM animals INNER JOIN species ON species_id = species.id WHERE species.name = 'Pokemon';
+SELECT owners.full_name, name FROM animals RIGHT JOIN owners ON owner_id = owners.id;
+SELECT species.name, COUNT(*) FROM animals INNER JOIN species ON species_id = species.id GROUP BY species.name;
+SELECT owners.full_name, species.name, animals.name FROM animals INNER JOIN owners ON owner_id = owners.id JOIN species ON species_id = species.id WHERE owners.full_name = 'Jennifer Orwell' and species.name = 'Digimon';
+SELECT owners.full_name, animals.name FROM animals INNER JOIN owners ON owner_id = owners.id WHERE owners.full_name = 'Dean Winchester' and escape_attempts = 0;
+SELECT owners.full_name, COUNT(animals.name) FROM animals INNER JOIN owners ON owner_id = owners.id GROUP BY owners.full_name ORDER BY COUNT(animals.name) DESC;
