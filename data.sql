c548a1cc49d11ef919024e455548e14a24c0f9eb
IngDavidTM <david-1011@hotmail.com>
2022-12-08 09:34:45 -0500

    add data

--- data.sql
+++ data.sql
@@ -32,3 +32,32 @@ UPDATE animals SET owner_id = 3 WHERE name IN ('Devimon', 'Plantmon');
 UPDATE animals SET owner_id = 4 WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
 UPDATE animals SET owner_id = 5 WHERE name IN ('Angemon', 'Boarmon');
 
+INSERT INTO vets(name, age, date_of_graduation) VALUES('William Tatcher', 45, 'April 23, 2000');
+INSERT INTO vets(name, age, date_of_graduation) VALUES('Maisy Smith', 26, 'January 17, 2019');
+INSERT INTO vets(name, age, date_of_graduation) VALUES('Stephanie Mendez', 64, 'May 4, 1981');
+INSERT INTO vets(name, age, date_of_graduation) VALUES('Jack Harkness', 38, 'June 8, 2008');
+
+INSERT INTO specializations(species_id, vets_id)
+VALUES(1, 1), (2, 3), (1, 3), (2, 4);
+
+INSERT INTO visits(animals_id, vets_id, date_of_visit)
+VALUES(1, 1, 'May 24, 2020'),
+      (1, 3, 'July 22, 2020'),
+      (2, 4, 'February 2, 2021'),
+      (3, 2, 'January 5, 2020'),
+      (3, 2, 'March 8, 2020'),
+      (3, 2, 'May 14, 2020'),
+      (4, 3, 'May 4, 2021'),
+      (5, 4, 'February 24, 2021'),
+      (6, 2, 'December 21, 2019'),
+      (6, 1, 'August 10, 2020'),
+      (6, 3, 'April 7, 2021'),
+      (7, 3, 'September 29, 2019'),
+      (8, 4, 'October 3, 2020'),
+      (8, 4, 'November 4, 2020'),
+      (9, 2, 'January 24, 2019'),
+      (9, 2, 'May 15, 2019'),
+      (9, 2, 'February 27, 2020'),
+      (9, 2, 'August 3, 2020'),
+      (10, 3, 'May 24, 2020'),
+      (10, 1, 'January 11, 2021');
